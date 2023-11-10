1e7be2292bb3cbe7a48b352e678c270f670a8c46
Justin Lim <goodaccoustics@hotmail.com>
2023-11-10 22:19:31 +0800

    connect to s3 bucket

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
 # app.py
 import os
-data = {"Message":"This method converts an original image to a thumbnail."}
-print(data)
-print(os.environ)
+import boto3
+
+s3Bucket = os.environ['S3_BUCKET']
+s3Key = os.environ['S3_KEY']
+s3KeySplit = s3Key.split("/")
+s3FileName = s3KeySplit[1]
+
+print(f"Message":"This method converts an original image from {s3Bucket}/{s3Key} into a thumbnail.")
+
+s3 = boto3.resource('s3', region_name='us-west-2')
+origObject = s3.Object(bucket_name = s3Bucket, key = s3Key)
+
+print(origObject.bucket_name)
+print(origObject.key)
