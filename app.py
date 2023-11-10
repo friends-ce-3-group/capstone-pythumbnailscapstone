9033679f751a19b6a826d14fa82b69c41880c183
Justin Lim <goodaccoustics@hotmail.com>
2023-11-10 22:25:17 +0800

    fix bug in print statement

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ s3Key = os.environ['S3_KEY']
 s3KeySplit = s3Key.split("/")
 s3FileName = s3KeySplit[1]
 
-print(f"Message":"This method converts an original image from {s3Bucket}/{s3Key} into a thumbnail.")
+print(f'This method converts an original image from {s3Bucket}/{s3Key} into a thumbnail.')
 
 s3 = boto3.resource('s3', region_name='us-west-2')
 origObject = s3.Object(bucket_name = s3Bucket, key = s3Key)
