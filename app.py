bd6ff9d27f78aa4b05bf06f63e5697a93f9a5f4d
Justin Lim <goodaccoustics@hotmail.com>
2023-11-10 22:40:11 +0800

    download s3 image

--- app.py
+++ app.py
@@ -3,14 +3,13 @@ import os
 import boto3
 from PIL import Image
 
-print(f'This method converts an original image from {s3Bucket}/{s3Key} into a thumbnail.')
-
 s3Bucket = os.environ['S3_BUCKET']
 s3Key = os.environ['S3_KEY']
 s3KeySplit = s3Key.split("/")
 s3FileName = s3KeySplit[1]
 tmpFilePath = '/tmp/'+s3FileName
 
+print(f'This method converts an original image from {s3Bucket}/{s3Key} into a thumbnail.')
 
 s3 = boto3.resource('s3', region_name='us-west-2')
 # origObject = s3.Object(bucket_name = s3Bucket, key = s3Key)
